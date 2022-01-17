9757cbe05d8637af1893b98c6ca3382ca5fe12e5
Eric Lemes <eric_efl@hotmail.com>
2022-01-17 12:44:26 -0300

    add - front registrer

---
+++ Block/Form/Register/Registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Elemes\DataPrivacy\Block\Form\Register;
+
+class Registration extends \Magento\Directory\Block\Data
+{
+    
+}
